9e941df8bb7d4d967d8e8bf7e09009fb05564e95
PeterChav01 <sahdow_nfsc@yahoo.com.mx>
2020-11-16 12:37:30 -0600

    Implemented side nav tabs to create dashboard showing reports, employees, and stores with their subsection
    Co-authored-by: JacquelineFloresMendez <jfloresm.dev@gmail.com>
    Co-authored-by: YilKanda <yilkanda@hotmail.com>

--- clock/config/routes.rb
+++ clock/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'stores#index'
+  root 'main#index'
   resources :stores
   resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
