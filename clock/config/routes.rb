4d211dbe3ebb32de292d10953820e52c4459bf2d
PeterChav01 <sahdow_nfsc@yahoo.com.mx>
2020-11-18 15:37:34 -0600

    Dashboard, added small configuration to show the views for stores and employees when click on each section of the sidebar navigation
    Co-authored-by: JacquelineFloresMendez <jfloresm.dev@gmail.com>
    Co-authored-by: YilKanda <yilkanda@hotmail.com>

--- clock/config/routes.rb
+++ clock/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'main#index'
+  root 'stores#index'
   resources :stores
   resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
