fc952acfe9961fc6103ebbdaf6fd7b4f7122ecfe
PeterChav01 <sahdow_nfsc@yahoo.com.mx>
2020-11-30 15:22:48 -0600

    Code fix, Code checked with Rubocop and fixed offenses, solved a bug in report view
    Co-authored-by: JacquelineFloresMendez <jfloresm.dev@gmail.com>
    Co-authored-by: YilKanda <yilkanda@hotmail>

--- clock/app/models/report.rb
+++ clock/app/models/report.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Model Report
 class Report < ApplicationRecord
   belongs_to :employee
 end
--- clock/test/system/stores_test.rb
+++ clock/test/system/stores_test.rb
@@ -1,53 +1,56 @@
-require "application_system_test_case"
+# frozen_string_literal: true
 
+require 'application_system_test_case'
+
+# Store Test Helper
 class StoresTest < ApplicationSystemTestCase
   setup do
     @store = stores(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit stores_url
-    assert_selector "h1", text: "Stores"
+    assert_selector 'h1', text: 'Stores'
   end
 
-  test "creating a Store" do
+  test 'creating a Store' do
     visit stores_url
-    click_on "New Store"
+    click_on 'New Store'
 
-    fill_in "Address", with: @store.address
-    fill_in "Close time", with: @store.close_time
-    fill_in "Name", with: @store.name
-    fill_in "Open time", with: @store.open_time
-    fill_in "Phone", with: @store.phone
-    fill_in "Status", with: @store.status
-    click_on "Create Store"
+    fill_in 'Address', with: @store.address
+    fill_in 'Close time', with: @store.close_time
+    fill_in 'Name', with: @store.name
+    fill_in 'Open time', with: @store.open_time
+    fill_in 'Phone', with: @store.phone
+    fill_in 'Status', with: @store.status
+    click_on 'Create Store'
 
-    assert_text "Store was successfully created"
-    click_on "Back"
+    assert_text 'Store was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Store" do
+  test 'updating a Store' do
     visit stores_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "Address", with: @store.address
-    fill_in "Close time", with: @store.close_time
-    fill_in "Name", with: @store.name
-    fill_in "Open time", with: @store.open_time
-    fill_in "Phone", with: @store.phone
-    fill_in "Status", with: @store.status
-    click_on "Update Store"
+    fill_in 'Address', with: @store.address
+    fill_in 'Close time', with: @store.close_time
+    fill_in 'Name', with: @store.name
+    fill_in 'Open time', with: @store.open_time
+    fill_in 'Phone', with: @store.phone
+    fill_in 'Status', with: @store.status
+    click_on 'Update Store'
 
-    assert_text "Store was successfully updated"
-    click_on "Back"
+    assert_text 'Store was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a Store" do
+  test 'destroying a Store' do
     visit stores_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Store was successfully destroyed"
+    assert_text 'Store was successfully destroyed'
   end
 end
