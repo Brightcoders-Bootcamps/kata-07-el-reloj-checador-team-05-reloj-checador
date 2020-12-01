5a18668b5a71665c146c8a8962465bd86f12d20a
PeterChav01 <sahdow_nfsc@yahoo.com.mx>
2020-11-30 18:04:43 -0600

    Readme
    Co-authored-by: JacquelineFloresMendez <jfloresm.dev@gmail.com>
    Co-authored-by: YilKanda <yilkanda@hotmail>

--- clock/db/migrate/20201123201042_create_reports.rb
+++ clock/db/migrate/20201123201042_create_reports.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Create Reports Migration
-class CreateRseports < ActiveRecord::Migration[6.0]
+class CreateReports < ActiveRecord::Migration[6.0]
   def change
     create_table :reports do |t|
       t.belongs_to :employee, null: false, foreign_key: true
